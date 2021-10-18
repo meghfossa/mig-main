fd6cd042aa05b93c3f5b167e6e81fdad431a4b57
Megh <megh@fossa.com>
2021-10-18 15:18:58 -0600

    modifies code for ci testing

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("from main repo! with v1.1.2-alpha.1")
-	fmt.Println("Current time:")
+	fmt.Println("Current local time:")
 	now := time.Now()
 	fmt.Println(now)
 }
